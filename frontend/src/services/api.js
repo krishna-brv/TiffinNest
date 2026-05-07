f8900d947dcc0bb767b2eb5360a73bb1c568ddfd
krishna-brv <brahmadevararoopvenkatakrishna@gmail.com>
2026-05-07 11:42:14 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://tiffinnest.onrender.com/api',
 });
 
 // Add a request interceptor to add token to headers
